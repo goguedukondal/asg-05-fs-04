35a77c43889ec5ed572fc92e579ec33dcbb46239
GOGU EDUKONDAL <97788201+goguedukondal@users.noreply.github.com>
2023-04-26 17:47:53 +0530

    Update HomeComponent.js

--- src/Components/HomeComponent.js
+++ src/Components/HomeComponent.js
@@ -2,8 +2,9 @@ import React from 'react'
 
 function HomeComponent() {
   return (
-    <div>
-      <img src='https://th.bing.com/th/id/OIP.Ch5iLCZ_Z53GU6zipof9-gHaEd?pid=ImgDet&rs=1' alt='mountain' width='100%' height='540px'/>
+    <div style={{backgroundImage:
+"url('https://th.bing.com/th/id/OIP.Ch5iLCZ_Z53GU6zipof9-gHaEd?pid=ImgDet&rs=1')", width='100%' height='540px'}}>
+     <h1>HOME</h1>
     </div>
   )
 }
