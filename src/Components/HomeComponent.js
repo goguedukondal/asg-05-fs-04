c334824735d34b6320dd807cd87e6a7b311478e4
GOGU EDUKONDAL <97788201+goguedukondal@users.noreply.github.com>
2023-04-26 17:36:38 +0530

    Update and rename Home.js to HomeComponent.js

---
+++ src/Components/HomeComponent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function HomeComponent() {
+  return (
+    <div>
+      <img src='https://th.bing.com/th/id/OIP.Ch5iLCZ_Z53GU6zipof9-gHaEd?pid=ImgDet&rs=1' alt='mountain' width='100%' height='540px'/>
+    </div>
+  )
+}
+
+export default HomeComponent
